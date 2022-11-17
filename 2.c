095c1b91320c29ec5eef958d20c0836042955309
Boy_With_No_Heart_007 <118186513+BoyWithNoHeart007@users.noreply.github.com>
2022-11-17 18:29:24 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main(){
+	
+	int no = 0;
+	
+	
+	if(0>no){
+		printf("No. is Positive");
+	}
+	
+	else if(0<no){ 
+		
+		printf("No. is Nagative");
+		
+	}
+	
+	else{
+		
+		printf("No. is Natural");
+		
+		
+	}
+	
+}
+	
